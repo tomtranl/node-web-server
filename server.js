81ae24645d04781bb3fb5f036595a5ceaa4b7c80
Tom Tran <swdande@gmail.com>
2018-08-14 17:24:48 -0700

    Add project page

--- server.js
+++ server.js
@@ -53,6 +53,14 @@ app.get('/about', (req, res) =>
     });
 });
 
+app.get('/projects', (req, res) =>
+{
+    res.render('projects.hbs', 
+    {
+        pageTitle: 'Projects'
+    });
+})
+
 // /bad --> send back json with errorMessage
 app.get('/bad', (req, res) =>
 {
